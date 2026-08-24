4d4c8c06193b8e8d2eb2e4282f7f809aaeb71bdf
Muthyalu Bhavesh <66527078+bhavesh16281@users.noreply.github.com>
2026-08-24 22:06:22 +0530

    first commit

---
+++ src/test/java/com/bhavesh16281/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavesh16281.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
